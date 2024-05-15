0484af4b4165977c531d8cb9e61a674592f22630
07anhad <anhadparashar07@gmail.com>
2024-05-15 20:38:07 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,34 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from './models/bookModel.js'
-import bookRoute from './routes/bookRoute.js'
-import cors from 'cors';
+import { Book } from "./models/bookModel.js";
+import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
-const app = express(); 
+const app = express();
 
 app.use(express.json());
 
 //middle ware for cors policy
-app.use(cors())
+app.use(
+  cors()
+);
 
-
-app.get('/', (request,response) => {
+app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send('Hello world')
-})
+  return response.status(234).send("Hello world");
+});
 
-app.use('/books', bookRoute);
+app.use("/books", bookRoute);
 
 mongoose
   .connect(mongoDBURL)
   .then(() => {
-    console.log('App connected to database');
+    console.log("App connected to database");
     app.listen(PORT, () => {
       console.log(`App is running on port ${PORT}`);
-    })
+    });
   })
   .catch((error) => {
     console.log(error);
-  }); 
+  });
